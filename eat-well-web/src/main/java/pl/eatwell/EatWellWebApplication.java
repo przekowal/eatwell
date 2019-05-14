cefaf14f96bbdd1920e9fac6825df690a90fbd26
Marcin <kowalski.marcin.r@gmail.com>
2019-05-14 14:25:50 +0200

    Refactor to use multi module: web-module

---
+++ eat-well-web/src/main/java/pl/eatwell/EatWellWebApplication.java
@@ -0,0 +1,13 @@
+package pl.eatwell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatWellWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatWellWebApplication.class, args);
+    }
+
+}
